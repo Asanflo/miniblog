dfc49680b820e38c2b33763eb4b1ff23f451bf53
Asanflo <agassemflorentin@gmail.com>
2025-05-02 02:47:54 +0100

    Conception de deux vues avec Django rest framework et test avec postman

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import listeArticles
+from .views import listeArticles, listapiview, listdetailapi
 
 urlpatterns = [
     path('listarticles/', listeArticles),
+    path('articles-list-api/', listapiview),
+    path('articles-listapi/<int:pk>/', listdetailapi),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 #chargement des models
 from .models import Articles
+from .serializers import ArticlesSerializer
 # Create your views here.
 
 
 def listeArticles(request):
     articles = Articles.objects.all()
     return render(request, "listArticles.html", {"articles":articles})
+
+@api_view(['GET'])
+def listapiview(request):
+    if request.method == 'GET':
+        articles = Articles.objects.all()
+        serializer = ArticlesSerializer(articles, many=True).data
+    return Response(serializer)
+
+@api_view(['GET'])
+def listdetailapi(request, pk):
+    article = get_object_or_404(Articles,pk=pk)
+    serializer = ArticlesSerializer(article, many=False).data
+    return Response(serializer)
