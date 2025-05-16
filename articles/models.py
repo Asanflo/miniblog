819291d945c1227955196661f43f85a89ef90cec
Asanflo <agassemflorentin@gmail.com>
2025-05-16 21:41:45 +0100

    Définition des nouveaux modèles utilisateur, catégori et commentaires

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,34 @@
+import os
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+class Userblog(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+
+def userAvatarContent(instance, filename):
+    extension = filename.split('.')[-1]
+    username = slugify(instance.user.username)
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = f"{username}_{timestamp}.{extension}"
+    return os.path.join('avatars',filename)
+
+class Category(models.Model):
+    nom = models.CharField(blank=False)
+    description =models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to=userAvatarContent, null=True, blank=True)
+
+    def __str__(self):
+        return self.nom
+
 
 class Articles(models.Model):
     title = models.CharField(max_length=100)
@@ -10,7 +37,20 @@ class Articles(models.Model):
     likes = models.IntegerField(blank=True)
     updated_at = models.DateTimeField(auto_now= True)
     published = models.BooleanField(default=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Userblog, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
+
+
+class CommentUser(models.Model):
+    content= models.TextField(blank=True)
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    author = models.ForeignKey(Userblog, on_delete=models.CASCADE)
+    comment_created = models.DateTimeField(auto_now_add=True)
+    comment_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.comment_update == None:
+            return f'De {self.author} le {self.comment_created}'
+        return f'De {self.author} le {self.comment_update}'
