8625e448ddb9c8f8dd0d330d949fc5cdcb144936
Asanflo <agassemflorentin@gmail.com>
2025-05-21 22:35:05 +0100

    Controle des donnees pour la modification des informations d'un utilisateur

--- articles/serializers.py
+++ articles/serializers.py
@@ -33,7 +33,41 @@ class UserblogSerializer(serializers.ModelSerializer):
         user_save = User.objects.create(**user_content) #creation de l'objet user en passant en parametre tout les arguments de user_content
         return Userblog.objects.create(user=user_save, **validated_data)
 
+    def update(self, instance, validated_data):
+        # Récupérez le nouveau username
+        new_username = validated_data.get('username', instance.username)
 
+        # Vérifiez si l'ID de l'instance correspond à celui dans validated_data
+        if instance.pk == validated_data.get('pk', instance.pk):
+            if instance.username != new_username:
+                # Vérifiez si le nouveau username existe déjà
+                if User.objects.filter(username=new_username).exists():
+                    raise serializers.ValidationError(
+                        {"username": "Un utilisateur avec ce nom d'utilisateur existe déjà."})
+                # Mettez à jour le username
+                instance.username = new_username
+            else:
+                # Réinitialisez les autres données selon le formulaire
+                instance.surname = validated_data.get('surname', instance.surname)
+                instance.email = validated_data.get('email', instance.email)
+
+                # Vérifiez les mots de passe
+                password = validated_data.get('password')
+                confirme_password = validated_data.get('confirmePassword')
+
+                if password and password != confirme_password:
+                    raise serializers.ValidationError({"password": "Les deux mots de passe ne correspondent pas."})
+
+                if password:
+                    instance.set_password(password)
+
+                # Vérifiez si un avatar a été fourni
+                avatar = validated_data.get('avatar')
+                if avatar is None and instance.avatar is None:
+                    raise serializers.ValidationError({"avatar": "Ce champ ne peut pas être nul."})
+
+        instance.save()  # Enregistrez les modifications
+        return instance
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
