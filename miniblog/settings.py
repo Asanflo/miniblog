ecd9944aa295f316f9f5edc64773326681242e74
Asanflo <agassemflorentin@gmail.com>
2025-05-28 01:28:49 +0100

    configuration de postgresql pour le projet

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'miniblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'miniblog_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
